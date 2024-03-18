6f4a71c60a7a1e34ab324b27b99a36ed58c158c6
Biju Borah <biju20_ug@ee.nits.ac.in>
2024-03-18 14:42:12 +0530

    server fixed

--- server.js
+++ server.js
@@ -5,6 +5,32 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000
 const app = express()
 const server = http.createServer(app)
+const fs = require('fs');
+
+function saveObjectToCSV(obj, filename) {
+    const csvContent = Object.values(obj).join(',') + '\n';
+
+    fs.access(filename, fs.constants.F_OK, (err) => {
+        if (err) {
+            // File doesn't exist, create it and append the object
+            fs.writeFile(filename, csvContent, 'utf8', (err) => {
+                if (err) {
+                    console.error('Error creating CSV file:', err);
+                    return;
+                }
+            });
+        } else {
+            // File exists, append the object
+            fs.appendFile(filename, csvContent, 'utf8', (err) => {
+                if (err) {
+                    console.error('Error appending to CSV file:', err);
+                    return;
+                }
+            });
+        }
+    });
+}
+
 const io = socketIo(server, {
     cors: {
         origin: 'http://localhost:5173'
@@ -20,28 +46,42 @@ io.on('connection', (socket) => {
     })
 })
 
-const graphData = [
-    { name: 1, x: Math.random() * 10, y: Math.random() * 10 },
-    { name: 2, x: Math.random() * 10, y: Math.random() * 10 },
-    { name: 3, x: Math.random() * 10, y: Math.random() * 10 },
-    { name: 4, x: Math.random() * 10, y: Math.random() * 10 },
-    { name: 5, x: Math.random() * 10, y: Math.random() * 10 },
-]
-setInterval(() => {
-    if (graphData.length >= 5) {
-        graphData.reverse().pop()
-        graphData.reverse()
-    }
-    graphData.push({ name: graphData[graphData.length - 1].name + 1, x: Math.random() * 10, y: Math.random() * 10 })
-    io.to('clock-room').emit('time', graphData)
-}, 1000)
 
-data = []
+setInterval(() => {
+    if (data.length > 0) {
+        if (graphData.length >= 20) {
+            graphData.reverse().pop()
+            graphData.reverse()
+        }
+        if (index < data.length) {
+            graphData.push(data[index])
+            saveObjectToCSV(data[index], 'output2.csv');
+            index += 1;
+        }
+    }
+    io.to('clock-room').emit('time', graphData)
+}, 30)
+
+var data = []
+var index = 0
+var y = 0
+var graphData = []
 
 app.get('/ecg', (req, res) => {
-    const ecgData = req.query.data;
-    data.push(ecgData);
-    console.log('Received ECG data:', ecgData);
+    const dataString = req.query.data;
+    const dataArray = dataString.split(',').map(Number);
+    console.log(dataArray);
+
+    for (let i = 0; i < dataArray.length; i++) {
+        const ecgData = {
+            name: y++,
+            x: dataArray[i],
+            y: Math.random() * 10
+        };
+        data.push(ecgData);
+    }
+
+    console.log('Received ECG data:', data);
     res.send({ message: 'ECG data received successfully' });
 });
 
